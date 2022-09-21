694c33e3a3b988d6226a4ec13aa60d48aadd11be
PEI FEN LIM <limpeifen0414@gmail.com>
2022-09-21 19:21:11 +0800

    init

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cczba6lgzvfo.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limszeyi-employee"
+customregion = "us-east-1"
